30df2c1980114b52e9ced3d6a26ce3a6516b230a
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2024-09-09 13:44:35 -0400

    Set up providers

--- providers.tf
+++ providers.tf
@@ -1,6 +1,21 @@
+# This is the "default" provider that is used assume the roles in the other
+# providers.  It uses the credentials of the caller.  It is also used to
+# assume the roles required to access remote state in the Terraform backend.
 provider "aws" {
   default_tags {
     tags = var.tags
   }
+
   region = var.aws_region
+}
+
+# This is the provider that is used to create resources inside the CyHy account.
+provider "aws" {
+  alias = "provisionaccount"
+  default_tags {
+    tags = var.tags
+  }
+
+  profile = "cool-cyhy-provisionaccount"
+  region  = var.aws_region
 }
