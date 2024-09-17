e7757f562a97ba850cf51b28ee5adccf9ab57d1a
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2024-09-17 10:46:39 -0400

    Add SG rules to allow the EC2 instance to communicate with the DocumentDB cluster

--- documentdb.tf
+++ documentdb.tf
@@ -16,3 +16,27 @@ module "documentdb-cluster" {
   tags            = var.tags
   vpc_id          = module.vpc.vpc_id
 }
+
+# Allow ingress from the EC2 instance to the DocumentDB cluster
+resource "aws_security_group_rule" "ingress_from_ec2_to_documentdb" {
+  provider = aws.provisionaccount
+
+  from_port                = var.db_port
+  protocol                 = "tcp"
+  security_group_id        = module.documentdb-cluster.security_group_id
+  source_security_group_id = module.ec2.security_group_id
+  to_port                  = var.db_port
+  type                     = "ingress"
+}
+
+# Allow egress from the EC2 instance to the DocumentDB cluster
+resource "aws_security_group_rule" "egress_from_ec2_to_documentdb" {
+  provider = aws.provisionaccount
+
+  from_port                = var.db_port
+  protocol                 = "tcp"
+  security_group_id        = module.ec2.security_group_id
+  source_security_group_id = module.documentdb-cluster.security_group_id
+  to_port                  = var.db_port
+  type                     = "egress"
+}
