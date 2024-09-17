a7a1b99fd94c1d6c691b83edaa09e8965d19005b
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2024-09-17 10:57:41 -0400

    Create a Lambda that runs the cyhy-kevsync-lambda code on a schedule

---
+++ cyhy-kevsync-lambda.tf
@@ -0,0 +1,76 @@
+# Create a Lambda function that runs the cyhy-kevsync-lambda code on a schedule.
+#
+# Prerequisites:
+# - A cyhy-kevsync Lambda deployment package stored in an S3 bucket (see the
+#   kevsync_lambda_s3_bucket and kevsync_lambda_s3_key variables)
+# - A valid CyHy configuration stored in the Systems Manager (SSM) Parameter
+#   Store of the Cyber Hygiene account (see the kevsync_lambda_config_ssm_key
+#   variable)
+
+module "kevsync_lambda" {
+  providers = {
+    aws = aws.provisionaccount
+  }
+
+  source  = "terraform-aws-modules/lambda/aws"
+  version = "7.9.0"
+
+  allowed_triggers = {
+    kevsync = {
+      principal  = "events.amazonaws.com"
+      source_arn = aws_cloudwatch_event_rule.kevsync_lambda_schedule.arn
+    }
+  }
+  attach_network_policy    = true
+  attach_policy_statements = true
+
+  # This is necessary to avoid the following error:
+  # "InvalidParameterValueException: We currently do not support adding policies
+  # for $LATEST." For more, see
+  # https://github.com/terraform-aws-modules/terraform-aws-lambda/blob/v7.9.0/README.md#faq
+  create_current_version_allowed_triggers = false
+
+  create_package        = false
+  description           = var.kevsync_lambda_description
+  environment_variables = merge({ "CYHY_CONFIG_SSM_PATH" = var.kevsync_lambda_config_ssm_key }, var.kevsync_lambda_env_variables)
+  function_name         = var.kevsync_lambda_name
+  handler               = var.kevsync_lambda_handler
+  policy_statements = {
+    ssm_read = {
+      effect    = "Allow",
+      actions   = ["ssm:GetParameter"],
+      resources = ["arn:aws:ssm:${var.aws_region}:${local.cyhy_account_id}:parameter${var.kevsync_lambda_config_ssm_key}"]
+    },
+  }
+  runtime = var.kevsync_lambda_runtime
+  s3_existing_package = {
+    bucket = var.kevsync_lambda_s3_bucket
+    key    = var.kevsync_lambda_s3_key
+  }
+  tags    = var.tags
+  timeout = var.kevsync_lambda_timeout
+  # TODO: Decide if we need the EC2 instance or not.  If not, we need to create a security group for the Lambda function.
+  vpc_security_group_ids = [module.ec2.security_group_id]
+  vpc_subnet_ids         = module.subnets.private_subnet_ids
+}
+
+# Schedule the Lambda function to run every on X minutes
+resource "aws_cloudwatch_event_rule" "kevsync_lambda_schedule" {
+  provider = aws.provisionaccount
+
+  description         = format("Executes %s Lambda every %d minute(s).", module.kevsync_lambda.lambda_function_name, var.kevsync_lambda_schedule_interval)
+  name                = format("%s-every-%d-minutes", module.kevsync_lambda.lambda_function_name, var.kevsync_lambda_schedule_interval)
+  schedule_expression = format("rate(%d minute%s)", var.kevsync_lambda_schedule_interval, var.kevsync_lambda_schedule_interval != 1 ? "s" : "")
+}
+
+resource "aws_cloudwatch_event_target" "kevsync_lambda_schedule" {
+  provider = aws.provisionaccount
+
+  arn = module.kevsync_lambda.lambda_function_arn
+  input = jsonencode({
+    detail-type = "Scheduled Event"
+    source      = "aws.events"
+    task        = "sync"
+  })
+  rule = aws_cloudwatch_event_rule.kevsync_lambda_schedule.name
+}
