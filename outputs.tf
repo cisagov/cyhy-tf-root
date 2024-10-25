7c3a243b350371a8ed510273dbbd2bf5e2ca4bd6
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2024-10-25 14:21:08 -0400

    Add some potentially-useful outputs

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,26 @@
-# TODO: Decide if we want any outputs
+output "documentdb_arn" {
+  description = "The ARN of the DocumentDB cluster."
+  value       = module.documentdb-cluster.arn
+}
+output "documentdb_endpoint" {
+  description = "The endpoint of the DocumentDB cluster."
+  value       = module.documentdb-cluster.endpoint
+}
+output "documentdb_sg_arn" {
+  description = "The ARN of the DocumentDB cluster security group."
+  value       = module.documentdb-cluster.security_group_arn
+}
+
+output "ec2" {
+  description = "The EC2 instance that is allowed to access the DocumentDB cluster."
+  value       = module.ec2
+}
+
+output "subnets" {
+  description = "The subnets within the CyHy VPC."
+  value       = module.subnets
+}
+output "vpc" {
+  description = "The CyHy VPC."
+  value       = module.vpc
+}
